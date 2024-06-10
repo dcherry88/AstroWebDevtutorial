d88e4e389d530b0b6f373d66fa2a8c45f674441e
Danny Cherry <dcherry@outlook.com>
2024-06-10 15:18:00 -0500

    more stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
-site: "https://starlit-sherbet-125aa4.netlify.app/"
+export default defineConfig({
+    site: "https://starlit-sherbet-125aa4.netlify.app"
+
+});
 
